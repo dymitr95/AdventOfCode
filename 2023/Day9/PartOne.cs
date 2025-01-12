38362bb76cfb6c0311c6263ed599850c1e5f79ef
Dymitr Dragalow <d.dragalow@gmail.com>
2025-01-12 22:19:42 +0100

    2023 - Day 9 - Part One in progress

---
+++ 2023/Day9/PartOne.cs
@@ -0,0 +1,15 @@
+﻿namespace Day9;
+
+public class PartOne
+{
+
+    public int Run(string input)
+    {
+        var result = 0;
+
+
+
+        return result;
+    }
+    
+}
---
+++ 2023/Day9/Program.cs
@@ -0,0 +1,19 @@
+﻿using Day9;
+
+const string inputFilePath = "./Input/input.txt";
+
+using var reader = new StreamReader(inputFilePath);
+
+var dataString = reader.ReadToEnd();
+reader.Close();
+
+
+
+var partOne = new PartOne();
+var firstResult = partOne.Run(dataString);
+
+// var partTwo = new PartTwo();
+// var secondResult = partTwo.Run(dataString);
+
+Console.WriteLine($"First result: {firstResult}");
+// Console.WriteLine($"Second result: {secondResult}");
