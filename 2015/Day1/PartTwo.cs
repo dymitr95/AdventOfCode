1fe3a83aac4e3aaddc98314c7af73a555e7907b8
Dymitr Dragalow <d.dragalow@gmail.com>
2025-01-13 23:58:53 +0100

    2015 - Day 1 - Part Two solution

---
+++ 2015/Day1/PartTwo.cs
@@ -0,0 +1,31 @@
+﻿namespace Day1;
+
+public class PartTwo
+{
+    public int Run(string input)
+    {
+        var result = 0;
+        var position = 0;
+
+        foreach (var parenthesis in input)
+        {
+            position++;
+            if (parenthesis == '(')
+            {
+                result++;
+            }
+            else
+            {
+                result--;
+            }
+
+            if (result == -1)
+            {
+                return position;
+            }
+        }
+
+
+        return position;
+    }
+}
--- 2015/Day1/obj/Debug/net8.0/Day1.AssemblyInfo.cs
+++ 2015/Day1/obj/Debug/net8.0/Day1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d62a209ac6dbad23b65778270904056d5c239612")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86b6c061b70de93ce0df72b3342119e74857e38b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
