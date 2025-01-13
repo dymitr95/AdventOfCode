86b6c061b70de93ce0df72b3342119e74857e38b
Dymitr Dragalow <d.dragalow@gmail.com>
2025-01-13 23:56:39 +0100

    2015 - Day 1 - Part One solution

---
+++ 2015/Day1/PartOne.cs
@@ -0,0 +1,27 @@
+﻿namespace Day1;
+
+public class PartOne
+{
+
+    public int Run(string input)
+    {
+        var result = 0;
+
+
+        foreach (var parenthesis in input)
+        {
+            if (parenthesis == '(')
+            {
+                result++;
+            }
+            else
+            {
+                result--;
+            }
+        }
+
+
+        return result;
+    }
+
+}
---
+++ 2015/Day1/Program.cs
@@ -0,0 +1,18 @@
+﻿using Day1;
+
+const string inputFilePath = "./Input/input.txt";
+
+using var reader = new StreamReader(inputFilePath);
+
+var dataString = reader.ReadToEnd();
+reader.Close();
+
+
+
+var partOne = new PartOne();
+var firstResult = partOne.Run(dataString);
+Console.WriteLine($"First result: {firstResult}");
+
+// var partTwo = new PartTwo();
+// var secondResult = partTwo.Run(dataString);
+// Console.WriteLine($"Second result: {secondResult}");
