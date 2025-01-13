165b01acef5a979d83ffe57c1bd2b1328a643aa4
Dymitr Dragalow <d.dragalow@gmail.com>
2025-01-14 00:15:48 +0100

    2015 - Day 2 - Part Two solution

---
+++ 2015/Day2/PartTwo.cs
@@ -0,0 +1,33 @@
+﻿namespace Day2;
+
+public class PartTwo
+{
+    public ulong Run(string input)
+    {
+        ulong result = 0;
+
+
+        var cubes = GetCubes(input);
+
+        foreach (var cube in cubes)
+        {
+            var bow = cube.Length * cube.Height * cube.Width;
+            var numbers = new ulong[]{ cube.Length, cube.Width, cube.Height };
+            
+            var smallest = numbers.OrderBy(n => n).Take(2).ToArray();
+            var present = 2 * smallest[0] + 2 * smallest[1];
+            result += bow + present;
+        }
+
+
+        return result;
+    }
+
+
+
+    private List<Cube> GetCubes(string input)
+    {
+        var rows = input.Split("\r\n");
+        return rows.Select(row => row.Split("x")).Select(sides => new Cube(Convert.ToUInt64(sides[1]), Convert.ToUInt64(sides[0]), Convert.ToUInt64(sides[2]))).ToList();
+    }
+}
--- 2015/Day2/obj/Debug/net8.0/Day2.AssemblyInfo.cs
+++ 2015/Day2/obj/Debug/net8.0/Day2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fe3a83aac4e3aaddc98314c7af73a555e7907b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ec7375502c5008c614987c85ba1f7a801aed163")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
