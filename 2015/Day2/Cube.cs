0ec7375502c5008c614987c85ba1f7a801aed163
Dymitr Dragalow <d.dragalow@gmail.com>
2025-01-14 00:08:55 +0100

    2015 - Day 2 - Part One solution

---
+++ 2015/Day2/Cube.cs
@@ -0,0 +1,15 @@
+﻿namespace Day2;
+
+public class Cube
+{
+    public ulong Width;
+    public ulong Length;
+    public ulong Height;
+
+    public Cube(ulong width, ulong length, ulong height)
+    {
+        Width = width;
+        Length = length;
+        Height = height;
+    }
+}
---
+++ 2015/Day2/PartOne.cs
@@ -0,0 +1,35 @@
+﻿namespace Day2;
+
+public class PartOne
+{
+    public ulong Run(string input)
+    {
+        ulong result = 0;
+
+
+        var cubes = GetCubes(input);
+
+        foreach (var cube in cubes)
+        {
+            var sides = new List<ulong>
+            {
+                cube.Length * cube.Width,
+                cube.Width * cube.Height,
+                cube.Height * cube.Length
+            };
+
+            result = result + 2 * sides[0] + 2 * sides[1] + 2 * sides[2] + sides.Min();
+        }
+
+
+        return result;
+    }
+
+
+
+    private List<Cube> GetCubes(string input)
+    {
+        var rows = input.Split("\r\n");
+        return rows.Select(row => row.Split("x")).Select(sides => new Cube(Convert.ToUInt64(sides[1]), Convert.ToUInt64(sides[0]), Convert.ToUInt64(sides[2]))).ToList();
+    }
+}
