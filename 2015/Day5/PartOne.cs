2ebec13442984a76806be66d26d3bfbed3ca5030
Dymitr Dragalow <d.dragalow@gmail.com>
2025-01-14 10:26:37 +0100

    2015 - Day 5 - Part One solution

---
+++ 2015/Day5/PartOne.cs
@@ -0,0 +1,57 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Day5;
+
+public class PartOne
+{
+
+
+    public int Run(string input)
+    {
+        var result = 0;
+
+        var words = input.Split("\r\n");
+
+        foreach (var word in words)
+        {
+            if (!ContainsMinThreeVowels(word))
+            {
+                continue;
+            }
+
+            if (!HasDoubleLetter(word))
+            {
+                continue;
+            }
+
+            if (ContainsForbiddenCharacters(word))
+            {
+                continue;
+            }
+
+            result++;
+        }
+
+        return result;
+    }
+
+
+    private bool ContainsMinThreeVowels(string input)
+    {
+        return input.ToLower().Count(c => "aeiou".Contains(c)) >= 3;
+    }
+
+
+    private bool HasDoubleLetter(string input)
+    {
+        const string pattern = @"(\w)\1";
+        return Regex.Match(input, pattern).Success;
+    }
+
+    private bool ContainsForbiddenCharacters(string input)
+    {
+        const string pattern = @"ab|cd|pq|xy";
+        return Regex.Match(input, pattern).Success;
+    }
+    
+}
