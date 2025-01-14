665e57db3fec725144cc15da5ecfcba08978e1e4
Dymitr Dragalow <d.dragalow@gmail.com>
2025-01-14 10:31:58 +0100

    2015 - Day 5 - Part Two solution

---
+++ 2015/Day5/PartTwo.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Day5;
+
+public class PartTwo
+{
+    public int Run(string input)
+    {
+        var result = 0;
+
+        var words = input.Split("\r\n");
+
+        foreach (var word in words)
+        {
+            if (!HasLetterBetween(word))
+            {
+                continue;
+            }
+
+            if (!HasPairOfTwo(word))
+            {
+                continue;
+            }
+
+            result++;
+        }
+
+        return result;
+    }
+
+
+    private bool HasPairOfTwo(string input)
+    {
+        var pattern = @"(\w\w).*\1";
+        return Regex.Match(input, pattern).Success;
+    }
+
+    private bool HasLetterBetween(string input)
+    {
+        const string pattern = @"(\w).\1";
+        return Regex.Match(input, pattern).Success;
+    }
+}
--- 2015/Day5/obj/Debug/net8.0/Day5.AssemblyInfo.cs
+++ 2015/Day5/obj/Debug/net8.0/Day5.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+922320a2109b5bde6f75871eef4e6b4a5eea4a83")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ebec13442984a76806be66d26d3bfbed3ca5030")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
