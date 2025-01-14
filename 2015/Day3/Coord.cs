3fb62f824bf472bcea98292b24cf9cf69abd9109
Dymitr Dragalow <d.dragalow@gmail.com>
2025-01-14 09:32:25 +0100

    2015 - Day 3 - Part One solution

---
+++ 2015/Day3/Coord.cs
@@ -0,0 +1,13 @@
+﻿namespace Day3;
+
+public record struct Coord
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public Coord(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+}
---
+++ 2015/Day3/PartOne.cs
@@ -0,0 +1,36 @@
+﻿namespace Day3;
+
+public class PartOne
+{
+    public int Run(string input)
+    {
+        var result = 0;
+
+        var coords = new HashSet<Coord>();
+
+        var x = 0;
+        var y = 0;
+
+        foreach (var way in input)
+        {
+            coords.Add(new Coord(x, y));
+            switch (way)
+            {
+                case '^':
+                    y += 1;
+                    break;
+                case 'v':
+                    y -= 1;
+                    break;
+                case '<':
+                    x -= 1;
+                    break;
+                case '>':
+                    x += 1;
+                    break;
+            }
+        }
+
+        return coords.Count;
+    }
+}
