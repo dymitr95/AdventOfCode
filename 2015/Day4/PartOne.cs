a61263c3405f2254293d02efe14ca7f059e52e57
Dymitr Dragalow <d.dragalow@gmail.com>
2025-01-14 09:51:12 +0100

    2015 - Day 4 - Part One solution

---
+++ 2015/Day4/PartOne.cs
@@ -0,0 +1,45 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Day4;
+
+public class PartOne
+{
+
+    public int Run(string input)
+    {
+        var result = 0;
+
+
+        while (true)
+        {
+            var data = input + result;
+            var hash = GetMd5Hash(data);
+            if (hash.StartsWith("00000"))
+            {
+                break;
+            }
+
+            result++;
+        }
+
+        return result;
+    }
+
+
+    private string GetMd5Hash(string input)
+    {
+        var inputBytes = Encoding.UTF8.GetBytes(input);
+        var hashBytes = MD5.HashData(inputBytes);
+
+        var sb = new StringBuilder();
+        foreach (var b in hashBytes)
+        {
+            sb.Append(b.ToString("x2"));
+        }
+
+        var hash = sb.ToString();
+        return hash;
+    }
+
+}
---
+++ 2015/Day4/Program.cs
@@ -0,0 +1,16 @@
+﻿// const string inputFilePath = "./Input/input.txt";
+//
+// using var reader = new StreamReader(inputFilePath);
+//
+// var dataString = reader.ReadToEnd();
+// reader.Close();
+
+using Day4;
+
+var partOne = new PartOne();
+var firstResult = partOne.Run("yzbqklnj");
+Console.WriteLine($"First result: {firstResult}");
+
+// var partTwo = new PartTwo();
+// var secondResult = partTwo.Run(dataString);
+// Console.WriteLine($"Second result: {secondResult}");
