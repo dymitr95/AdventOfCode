7cea6d40fb2ff655aaffb6a36011b881fa52afa0
Dymitr Dragalow <d.dragalow@gmail.com>
2025-01-14 22:39:37 +0100

    2016 - Day 1 - Part One solution

--- 2016/2016/Day1/Day1Part1.cs
+++ 2016/2016/Day1/Day1Part1.cs
@@ -4,6 +4,70 @@ public class Day1Part1 : Part<int>
 {
     public override int Run(string input)
     {
-        return 1;
+        var moves = GetMoves(input);
+
+        var x = 0;
+        var y = 0;
+
+        var orientation = 'N';
+
+        foreach (var move in moves)
+        {
+            var direction = move[0].ToString();
+            var steps = Convert.ToInt32(move.Substring(1, move.Length - 1));
+
+            if (direction == "L")
+            {
+                switch (orientation)
+                {
+                    case 'N':
+                        orientation = 'W';
+                        x -= steps;
+                        break;
+                    case 'S':
+                        orientation = 'E';
+                        x += steps;
+                        break;
+                    case 'W':
+                        orientation = 'S';
+                        y -= steps;
+                        break;
+                    case 'E':
+                        orientation = 'N';
+                        y += steps;
+                        break;
+                }
+            }
+            else
+            {
+                switch (orientation)
+                {
+                    case 'N':
+                        orientation = 'E';
+                        x += steps;
+                        break;
+                    case 'S':
+                        orientation = 'W';
+                        x -= steps;
+                        break;
+                    case 'W':
+                        orientation = 'N';
+                        y += steps;
+                        break;
+                    case 'E':
+                        orientation = 'S';
+                        y -= steps;
+                        break;
+                }
+            }
+        }
+        
+        return Math.Abs(x) + Math.Abs(y);
+    }
+
+
+    private List<string> GetMoves(string input)
+    {
+        return input.Split(", ").ToList();
     }
 }
--- 2016/2016/obj/Debug/net8.0/2016.AssemblyInfo.cs
+++ 2016/2016/obj/Debug/net8.0/2016.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("2016")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f4a1e8eef43a5c2bba213c6ea5f5d231b070306")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6724a0ae64ecdb45695ad6ddd9faac38f566a821")]
 [assembly: System.Reflection.AssemblyProductAttribute("2016")]
 [assembly: System.Reflection.AssemblyTitleAttribute("2016")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
