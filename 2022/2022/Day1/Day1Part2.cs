765e549c2d52bb27417d10894a2b337f79deaf8f
Dymitr Dragalow <d.dragalow@gmail.com>
2025-01-17 11:12:50 +0100

    2022 - Day1 - Part Two solution

--- 2022/2022/Day1/Day1Part2.cs
+++ 2022/2022/Day1/Day1Part2.cs
@@ -4,6 +4,27 @@ public class Day1Part2 : Part<int>
 {
     public override int Run(string input)
     {
-        throw new NotImplementedException();
+        var result = 0;
+
+        var rows = input.Split("\r\n");
+
+        var results = new List<int>();
+        var temp = 0;
+        foreach (var row in rows)
+        {
+            if (row == "")
+            {
+                results.Add(temp);
+                temp = 0;
+                continue;
+            }
+
+            temp += Convert.ToInt32(row);
+        }
+
+        results = results.OrderByDescending(v => v).ToList();
+
+        return results[0] + results[1] + results[2];
     }
 }
+
