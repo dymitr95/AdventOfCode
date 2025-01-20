f6747525d97aa09a25ad97dff022fd6007fd2c38
Dymitr Dragalow <d.dragalow@gmail.com>
2025-01-20 12:57:31 +0100

    2021 prepared

---
+++ 2021/2021/Day.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+
+namespace _2021;
+
+
+public class Day<T1,T2>
+{
+    private string Input { get; }
+    private double TotalTime { get; set; }
+    
+    public Day(string inputPath)
+    {
+        Input = GetInput(inputPath);
+    }
+
+    public double RunPartOne(Part<T1> partOne)
+    {
+        var stopwatch = Stopwatch.StartNew();
+        Console.WriteLine($"First result: {partOne.Run(Input)}");
+        stopwatch.Stop();
+        Console.WriteLine($"Runtime: {stopwatch.Elapsed.TotalMilliseconds:F3} ms");
+        return stopwatch.Elapsed.TotalMilliseconds;
+    }
+
+    public double RunPartTwo(Part<T2> partTwo)
+    {
+        var stopwatch = Stopwatch.StartNew();
+        Console.WriteLine($"Second result: {partTwo.Run(Input)}");
+        stopwatch.Stop();
+        Console.WriteLine($"Runtime: {stopwatch.Elapsed.TotalMilliseconds:F3} ms");
+        return stopwatch.Elapsed.TotalMilliseconds;
+    }
+
+    private static string GetInput(string inputPath)
+    {
+        using var reader = new StreamReader(inputPath);
+        var dataString = reader.ReadToEnd();
+        reader.Close();
+        return dataString;
+    }
+}
---
+++ 2021/2021/Part.cs
@@ -0,0 +1,6 @@
+﻿namespace _2021;
+
+public abstract class Part<T>
+{
+    public abstract T Run(string input);
+}
