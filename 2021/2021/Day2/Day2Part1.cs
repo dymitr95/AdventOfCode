221120402a57082a734c9d04fa59e2df20685039
Dymitr Dragalow <d.dragalow@gmail.com>
2025-01-21 09:51:46 +0100

    2021 - Day2 - Part One solution

---
+++ 2021/2021/Day2/Day2Part1.cs
@@ -0,0 +1,33 @@
+﻿namespace _2021.Day2;
+
+public class Day2Part1 : Part<int>
+{
+    public override int Run(string input)
+    {
+        var result = 0;
+        var rows = input.Split("\r\n");
+
+        var horizontal = 0;
+        var depth = 0;
+        
+        foreach (var row in rows)
+        {
+            var data = row.Split(" ");
+            var value = Convert.ToInt32(data[1]);
+            switch (data[0])
+            {
+                case "forward":
+                    horizontal += value;
+                    break;
+                case "up":
+                    depth -= value;
+                    break;
+                case "down":
+                    depth += value;
+                    break;
+            }
+        }
+
+        return horizontal * depth;
+    }
+}
--- 2021/2021/Program.cs
+++ 2021/2021/Program.cs
@@ -1,5 +1,6 @@
 ﻿using _2021;
 using _2021.Day1;
+using _2021.Day2;
 
 var totalTime = 0.0;
 
@@ -8,6 +9,14 @@ Console.WriteLine("> Day 1 <");
 var day1 = new Day<int, int>("./Input/day1.txt");
 totalTime += day1.RunPartOne(new Day1Part1());
 totalTime += day1.RunPartTwo(new Day1Part2());
+Console.WriteLine(new string('-', 20));
+
+
+//DAY 2
+Console.WriteLine("> Day 2 <");
+var day2 = new Day<int, int>("./Input/day2.txt");
+totalTime += day2.RunPartOne(new Day2Part1());
+totalTime += day2.RunPartTwo(new Day2Part2());
 Console.WriteLine(new string('-', 20));
 
 Console.WriteLine($"Total runtime: {totalTime:F3} ms");
