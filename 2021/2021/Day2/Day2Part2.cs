a7e5827c8d21ec53c31dcf3316fb3b6d824b2e6d
Dymitr Dragalow <d.dragalow@gmail.com>
2025-01-21 09:55:23 +0100

    2021 - Day2 - Part Two solution

--- 2021/2021/Day2/Day2Part2.cs
+++ 2021/2021/Day2/Day2Part2.cs
@@ -4,6 +4,32 @@ public class Day2Part2 : Part<int>
 {
     public override int Run(string input)
     {
-        throw new NotImplementedException();
+        var result = 0;
+        var rows = input.Split("\r\n");
+
+        var horizontal = 0;
+        var depth = 0;
+        var aim = 0;
+        
+        foreach (var row in rows)
+        {
+            var data = row.Split(" ");
+            var value = Convert.ToInt32(data[1]);
+            switch (data[0])
+            {
+                case "forward":
+                    horizontal += value;
+                    depth += value * aim;
+                    break;
+                case "up":
+                    aim -= value;
+                    break;
+                case "down":
+                    aim += value;
+                    break;
+            }
+        }
+
+        return horizontal * depth;
     }
 }
--- 2021/2021/obj/Debug/net8.0/2021.AssemblyInfo.cs
+++ 2021/2021/obj/Debug/net8.0/2021.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("2021")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8836fa3f8e9e6d9af2b77136f661d6a2aa37cbd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+221120402a57082a734c9d04fa59e2df20685039")]
 [assembly: System.Reflection.AssemblyProductAttribute("2021")]
 [assembly: System.Reflection.AssemblyTitleAttribute("2021")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
