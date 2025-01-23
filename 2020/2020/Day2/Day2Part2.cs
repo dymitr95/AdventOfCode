a84dca9e6d2551ae48286e5d798b2421aa11c6f3
Dymitr Dragalow <d.dragalow@gmail.com>
2025-01-23 19:48:18 +0100

    2020 - Day2 - Part Two solution

--- 2020/2020/Day2/Day2Part2.cs
+++ 2020/2020/Day2/Day2Part2.cs
@@ -4,6 +4,29 @@ public class Day2Part2 : Part<int>
 {
     public override int Run(string input)
     {
-        throw new NotImplementedException();
+        var result = 0;
+
+        var rows = input.Split("\r\n");
+
+        foreach (var row in rows)
+        {
+
+            var data = row.Split(" ");
+            var posFirst = Convert.ToInt32(data[0].Split("-")[0]) - 1;
+            var posSecond = Convert.ToInt32(data[0].Split("-")[1]) - 1;
+
+            var password = data[2];
+            var symbol = data[1][0];
+
+            if (password[posFirst] == symbol && password[posSecond] != symbol ||
+                password[posFirst] != symbol && password[posSecond] == symbol)
+            {
+                result++;
+            }
+            
+        }
+
+
+        return result;
     }
 }
