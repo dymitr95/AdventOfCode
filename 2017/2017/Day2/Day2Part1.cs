468bd32c4576d02b6babc307d7e6cf6dba2b5c85
Dymitr Dragalow <d.dragalow@gmail.com>
2025-01-24 17:26:11 +0100

    2017 - Day2 - Part Two solution

---
+++ 2017/2017/Day2/Day2Part1.cs
@@ -0,0 +1,22 @@
+﻿namespace _2017.Day2;
+
+public class Day2Part1 : Part<int>
+{
+    public override int Run(string input)
+    {
+        var result = 0;
+
+        var rows = input.Split("\r\n");
+        foreach (var row in rows)
+        {
+            var numbers = row.Split("\t");
+            var list = numbers.Select(numb => Convert.ToInt32(numb)).ToList();
+            list.Sort();
+            result += list[^1] - list[0];
+        }
+
+
+        return result;
+    }
+    
+}
