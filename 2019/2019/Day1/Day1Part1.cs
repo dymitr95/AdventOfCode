92d9863375bd9b3f2b8ee925ed3ee9dbb3847ecb
Dymitr Dragalow <d.dragalow@gmail.com>
2025-01-24 16:58:43 +0100

    2019 - Day1- Part One solution

--- 2019/2019/Day1/Day1Part1.cs
+++ 2019/2019/Day1/Day1Part1.cs
@@ -4,6 +4,16 @@ public class Day1Part1 : Part<int>
 {
     public override int Run(string input)
     {
-        throw new NotImplementedException();
+        var result = 0;
+
+        var rows = input.Split("\r\n");
+
+        foreach (var row in rows)
+        {
+            var numb = Convert.ToInt32(row);
+            result += (int)Math.Floor((double)numb / 3) - 2;
+        }
+
+        return result;
     }
 }
