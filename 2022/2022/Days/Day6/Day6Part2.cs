8053fced2c2cc6becaa3cabc25501f85c11253e2
Dymitr Dragalow <d.dragalow@gmail.com>
2025-11-29 18:28:00 +0100

    2022 Day 6 Part Two

--- 2022/2022/Days/Day6/Day6Part2.cs
+++ 2022/2022/Days/Day6/Day6Part2.cs
@@ -6,7 +6,22 @@ public class Day6Part2 : Part<int>
 {
     public override int Run(string input)
     {
-        var rows = input.Split("\r\n");
+
+        for (var i = 0; i < input.Length - 13; i++)
+        {
+            var letters = new HashSet<char>();
+            
+            for (var j = i; j < i + 14; j++)
+            {
+                letters.Add(input[j]);
+            }
+
+            if (letters.Count == 14)
+            {
+                return i + 14;
+            }
+        }
+        
         return 0;
     }
 }
