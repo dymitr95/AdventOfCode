2e4b5823bbe7ddc3946123f133933cd882617d36
Dymitr Dragalow <d.dragalow@gmail.com>
2025-12-02 13:23:16 +0100

    2025 Day 3 preparation

---
+++ 2025/Days/Day03/Day3Part1.cs
@@ -0,0 +1,13 @@
+﻿using _2025.Structure;
+
+namespace _2025.Days.Day03;
+
+public class Day3Part1 : Part<int>
+{
+    public override int Run(string input)
+    {
+        var rows = input.Split("\r\n");
+        
+        return 0;
+    }
+}
--- 2025/Program.cs
+++ 2025/Program.cs
@@ -1,17 +1,29 @@
 ﻿using _2025.Days.Day01;
 using _2025.Days.Day02;
+using _2025.Days.Day03;
 using _2025.Structure;
 
 var totalTime = 0.0;
+var dayTotalTime = 0.0;
 
 // //DAY 1
 // var day1 = new Day<int, int>(1, new Day1Part1(), new Day1Part2());
-// totalTime += day1.Solve();
+// dayTotalTime = day1.Solve();
+// Console.WriteLine($"Day total runtime: {dayTotalTime:F3} ms");
+// totalTime += dayTotalTime;
+//
+// //DAY 2
+// var day2 = new Day<ulong, ulong>(2, new Day2Part1(), new Day2Part2());
+// dayTotalTime = day2.Solve();
+// Console.WriteLine($"Day total runtime: {dayTotalTime:F3} ms");
+// totalTime += dayTotalTime;
 
-//DAY 2
-var day2 = new Day<ulong, ulong>(2, new Day2Part1(), new Day2Part2());
-totalTime += day2.Solve();
+//DAY 3
+var day3 = new Day<int, int>(3, new Day3Part1(), new Day3Part2());
+dayTotalTime = day3.Solve();
+Console.WriteLine($"Day total runtime: {dayTotalTime:F3} ms");
+totalTime += dayTotalTime;
 
 
 Console.WriteLine(new string('-', 20));
-Console.WriteLine($"Total runtime: {totalTime:F3} ms");
+Console.WriteLine($"All days total runtime: {totalTime:F3} ms");
