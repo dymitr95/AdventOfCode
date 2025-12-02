fc30d6f428618aeefa279392c7bb05549c6c5eba
Dymitr Dragalow <d.dragalow@gmail.com>
2025-12-02 09:35:35 +0100

    2025 Day 2 Part 1 Better solution + tests

--- 2025/Days/Day02/Day2Part1.cs
+++ 2025/Days/Day02/Day2Part1.cs
@@ -18,7 +18,7 @@ public class Day2Part1 : Part<ulong>
 
             result += GetSumOfInvalidIdsInRange(start, end);
         }
-        
+
         return result;
     }
 
@@ -26,32 +26,73 @@ public class Day2Part1 : Part<ulong>
     {
         var result = 0UL;
 
-        for (var value = start; value <= end;)
+        var minimalInvalidId = GetMinimalInvalidId(start);
+        var maximumInvalidId = GetMaximumInvalidId(end);
+        var diff = GetDiff(minimalInvalidId.ToString().Length);
+
+        for (var value = minimalInvalidId; value <= end && value <= maximumInvalidId; value += diff)
         {
-            var valueStr = value.ToString();
-            if (valueStr.Length % 2 != 0)
-            {
-                value += 1;
-                continue;
-            }
-
-            var half = valueStr.Length / 2;
-            if (valueStr[..half] != valueStr[half..])
-            {
-                value += 1;
-                continue;
-            }
-
             result += value;
-
-            var hundreds = (ulong)(valueStr.Length / 100);
-            value += 10 * hundreds + 1;
         }
-        
-        
+
+
         return result;
     }
-    
-    
-    
+
+    private static ulong GetMinimalInvalidId(ulong value)
+    {
+        var startStr = value.ToString();
+
+        int targetLength;
+        if (startStr.Length % 2 != 0)
+        {
+            targetLength = startStr.Length + 1;
+        }
+        else
+        {
+            targetLength = startStr.Length;
+        }
+
+        var multiplier = (ulong)(targetLength / 2) - 1;
+        var minInvalidId = Convert.ToUInt64(string.Concat(Enumerable.Repeat(Math.Pow(10, multiplier), 2)));
+
+        if (minInvalidId >= value)
+        {
+            return minInvalidId;
+        }
+
+        var diff = GetDiff(targetLength);
+        var targetMultiplier = value / diff;
+        minInvalidId =  diff * targetMultiplier;
+
+        if (minInvalidId < value)
+        {
+            return minInvalidId + diff;
+        }
+
+        return minInvalidId;
+    }
+
+    private static ulong GetMaximumInvalidId(ulong value)
+    {
+        var endStr = value.ToString();
+
+        int targetLength;
+        if (endStr.Length % 2 != 0)
+        {
+            targetLength = endStr.Length - 1;
+        }
+        else
+        {
+            targetLength = endStr.Length;
+        }
+        
+        return Convert.ToUInt64(string.Concat(Enumerable.Repeat(9, targetLength)));
+    }
+
+    private static ulong GetDiff(int length)
+    {
+        var multiplier = (ulong)(length / 2);
+        return (ulong)Math.Pow(10, multiplier) + 1;
+    }
 }
---
+++ 2025/Tests/Days/Day02/Day2Part1Tests.cs
@@ -0,0 +1,96 @@
+using _2025.Days.Day02;
+
+namespace Tests;
+
+[TestFixture]
+public class Day2Part1Tests
+{
+
+    [Test]
+    public void Test1()
+    {
+        var part = new Day2Part1();
+        var result = part.Run("11-22");
+        Assert.That(result, Is.EqualTo(33));
+    }
+    
+    [Test]
+    public void Test2()
+    {
+        var part = new Day2Part1();
+        var result = part.Run("95-115");
+        Assert.That(result, Is.EqualTo(99));
+    }
+    
+    [Test]
+    public void Test3()
+    {
+        var part = new Day2Part1();
+        var result = part.Run("222220-222224");
+        Assert.That(result, Is.EqualTo(222222));
+    }
+    
+    [Test]
+    public void Test4()
+    {
+        var part = new Day2Part1();
+        var result = part.Run("998-1012");
+        Assert.That(result, Is.EqualTo(1010));
+    }
+    
+    [Test]
+    public void Test5()
+    {
+        var part = new Day2Part1();
+        var result = part.Run("1188511880-1188511890");
+        Assert.That(result, Is.EqualTo(1188511885));
+    }
+    
+    [Test]
+    public void Test6()
+    {
+        var part = new Day2Part1();
+        var result = part.Run("1698522-1698528");
+        Assert.That(result, Is.EqualTo(0));
+    }
+    
+    [Test]
+    public void Test7()
+    {
+        var part = new Day2Part1();
+        var result = part.Run("446443-446449");
+        Assert.That(result, Is.EqualTo(446446));
+    }
+    
+    [Test]
+    public void Test8()
+    {
+        var part = new Day2Part1();
+        var result = part.Run("38593856-38593862");
+        Assert.That(result, Is.EqualTo(38593859));
+    }
+    
+    [Test]
+    public void Test9()
+    {
+        var part = new Day2Part1();
+        var result = part.Run("565653-565659");
+        Assert.That(result, Is.EqualTo(0));
+    }
+    
+    [Test]
+    public void Test10()
+    {
+        var part = new Day2Part1();
+        var result = part.Run("824824821-824824827");
+        Assert.That(result, Is.EqualTo(0));
+    }
+    
+    [Test]
+    public void Test11()
+    {
+        var part = new Day2Part1();
+        var result = part.Run("2121212118-2121212124");
+        Assert.That(result, Is.EqualTo(0));
+    }
+}
