1accb3379a851169db4805632318b740a2b278dc
Dymitr Dragalow <d.dragalow@gmail.com>
2025-12-05 06:25:53 +0100

    2025 Day 1 Part 1

--- 2025/Days/Day05/Day5Part1.cs
+++ 2025/Days/Day05/Day5Part1.cs
@@ -8,8 +8,47 @@ public class Day5Part1 : Part<int>
     {
         var rows = input.Split("\r\n");
         var result = 0;
+
+        var ingredients = GetFreshIngredientsIds(rows);
+        
+        foreach(var row in rows)
+        {
+            if (row.Contains('-') || row == "")
+            {
+                continue;
+            }
+
+            var ingredient = long.Parse(row);
+            if (ingredients.FirstOrDefault(r => r.Min <= ingredient && r.Max >= ingredient) != null)
+            {
+                result++;
+            }
+        }
         
         return result;
     }
+
+    private static List<Range> GetFreshIngredientsIds(string[] rows)
+    {
+        var res = new List<Range>();
+
+        foreach (var row in rows)
+        {
+            if (row == "")
+            {
+                return res;
+            }
+
+            var data = row.Split("-");
+            var start = long.Parse(data[0]);
+            var end = long.Parse(data[1]);
+            
+            res.Add(new Range(start, end));
+        }
+
+        return res;
+    }
     
 }
+
+public record Range(long Min, long Max);
