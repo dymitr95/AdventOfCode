68f6b22dbb3625ec645533a00f5c1381b5131440
Dymitr Dragalow <d.dragalow@gmail.com>
2025-12-11 06:58:53 +0100

    2025 Day 11 Part 1

--- 2025/Days/Day11/Day11Part1.cs
+++ 2025/Days/Day11/Day11Part1.cs
@@ -8,7 +8,86 @@ public class Day11Part1 : Part<int>
     {
         var rows = input.Split("\r\n");
         var result = 0;
-        return result;
+
+        var nodes = ParseInput(rows);
+
+        var startNode = nodes.First(n => n.Value == "you");
+        var targetNode = nodes.First(n => n.Value == "out");
+
+        var paths = new List<List<string>>();
+
+        FindPaths(startNode, targetNode, nodes, [], [], paths);
+
+        return paths.Count;
     }
-    
+
+
+    private static void FindPaths(Node node, Node target, List<Node> graph,
+        List<string> visitedNodes, List<string> path, List<List<string>> paths)
+    {
+        path.Add(node.Value);
+        visitedNodes.Add(node.Value);
+
+        if (node.Value == target.Value)
+        {
+            paths.Add([..path]);
+            path.Remove(node.Value);
+            visitedNodes.Remove(node.Value);
+            return;
+        }
+
+        foreach (var child in node.ChildrenNodes)
+        {
+            if (visitedNodes.Contains(child))
+            {
+                continue;
+            }
+
+            var childNode = graph.First(n => n.Value == child);
+            FindPaths(childNode, target, graph, visitedNodes, path, paths);
+        }
+
+        path.Remove(node.Value);
+        visitedNodes.Remove(node.Value);
+    }
+
+
+    private static List<Node> ParseInput(string[] rows)
+    {
+        var nodes = new List<Node>();
+
+        foreach (var row in rows)
+        {
+            var data = row.Split(": ");
+            var node = new Node(data[0]);
+            var children = data[1].Split(" ");
+
+            if (nodes.FirstOrDefault(n => n.Value == node.Value) != null)
+            {
+                node = nodes.FirstOrDefault(n => n.Value == node.Value);
+            }
+            else
+            {
+                nodes.Add(node);
+            }
+
+            foreach (var childNode in children)
+            {
+                if (nodes.FirstOrDefault(n => n.Value == childNode) == null)
+                {
+                    nodes.Add(new Node(childNode));
+                }
+
+                node.ChildrenNodes.Add(childNode);
+            }
+        }
+
+        return nodes;
+    }
+}
+
+public class Node(string value)
+{
+    public string Value { get; set; } = value;
+    public List<string> ChildrenNodes { get; set; } = [];
 }
