203abae1420d5f867eba318b061e6e89768f106a
Dymitr Dragalow <d.dragalow@gmail.com>
2025-12-12 08:53:33 +0100

    2025 Day 12 Part 1

--- 2025/Days/Day12/Day12Part1.cs
+++ 2025/Days/Day12/Day12Part1.cs
@@ -6,7 +6,76 @@ public class Day12Part1 : Part<int>
 {
     public override int Run(string input)
     {
-        var rows = input.Split("\r\n");
-        return 0;
+        var (shapes, regions) = ParseInput(input);
+        var solvableCount = 0;
+
+        foreach (var (maxCells, dict) in regions)
+        {
+            var cellsNeeded = dict.Keys.Sum(shapeId => shapes[shapeId] * dict[shapeId]);
+
+            if (maxCells >= cellsNeeded)
+            {
+                solvableCount++;
+            }
+
+        }
+
+        return solvableCount;
+    }
+
+    private static (Dictionary<int, int>, List<(int, Dictionary<int,int>)>) ParseInput(string input)
+    {
+        var lines = input.Split('\n').Select(l => l.Trim()).Where(l => !string.IsNullOrEmpty(l)).ToList();
+        var shapes = new Dictionary<int, int>();
+        var regions = new List<(int, Dictionary<int, int>)>();
+
+        var i = 0;
+        while (i < lines.Count)
+        {
+            if (lines[i].Contains(':') && !lines[i].Contains('x'))
+            {
+                var shapeIndex = int.Parse(lines[i].TrimEnd(':'));
+                var cells = 0;
+                i++;
+
+                while (i < lines.Count && !lines[i].Contains(':'))
+                {
+                    for (var col = 0; col < lines[i].Length; col++)
+                    {
+                        if (lines[i][col] == '#')
+                        {
+                            cells += 1;
+                        }
+                    }
+
+                    i++;
+                }
+
+                shapes.Add(shapeIndex, cells);
+            }
+            else if (lines[i].Contains('x'))
+            {
+                var parts = lines[i].Split(':');
+                var dims = parts[0].Split('x');
+                var width = int.Parse(dims[0]);
+                var height = int.Parse(dims[1]);
+
+                var counts = parts[1].Trim().Split(' ').Select(int.Parse).ToList();
+                var requiredShapes = new Dictionary<int, int>();
+
+                for (var j = 0; j < counts.Count; j++)
+                {
+                    if (counts[j] > 0)
+                    {
+                        requiredShapes[j] = counts[j];
+                    }
+                }
+
+                regions.Add((width * height, requiredShapes));
+                i++;
+            }
+        }
+
+        return (shapes, regions);
     }
 }
--- 2025/Program.cs
+++ 2025/Program.cs
@@ -15,74 +15,74 @@ using _2025.Structure;
 var totalTime = 0.0;
 var dayTotalTime = 0.0;
 
-// //DAY 1
-// var day1 = new Day<int, int>(1, new Day1Part1(), new Day1Part2());
-// dayTotalTime = day1.Solve();
-// Console.WriteLine($"Day total runtime: {dayTotalTime:F3} ms");
-// totalTime += dayTotalTime;
-//
-// //DAY 2
-// var day2 = new Day<ulong, ulong>(2, new Day2Part1(), new Day2Part2());
-// dayTotalTime = day2.Solve();
-// Console.WriteLine($"Day total runtime: {dayTotalTime:F3} ms");
-// totalTime += dayTotalTime;
-//
-// //DAY 3
-// var day3 = new Day<int, long>(3, new Day3Part1(), new Day3Part2());
-// dayTotalTime = day3.Solve();
-// Console.WriteLine($"Day total runtime: {dayTotalTime:F3} ms");
-// totalTime += dayTotalTime;
-//
-// //DAY 4
-// var day4 = new Day<int, int>(4, new Day4Part1(), new Day4Part2());
-// dayTotalTime = day4.Solve();
-// Console.WriteLine($"Day total runtime: {dayTotalTime:F3} ms");
-// totalTime += dayTotalTime;
-//
-// //DAY 5
-// var day5 = new Day<int, long>(5, new Day5Part1(), new Day5Part2());
-// dayTotalTime = day5.Solve();
-// Console.WriteLine($"Day total runtime: {dayTotalTime:F3} ms");
-// totalTime += dayTotalTime;
-//
-// //DAY 6
-// var day6 = new Day<long, long>(6, new Day6Part1(), new Day6Part2());
-// dayTotalTime = day6.Solve();
-// Console.WriteLine($"Day total runtime: {dayTotalTime:F3} ms");
-// totalTime += dayTotalTime;
-//
-// //DAY 7
-// var day7 = new Day<int, long>(7, new Day7Part1(), new Day7Part2());
-// dayTotalTime = day7.Solve();
-// Console.WriteLine($"Day total runtime: {dayTotalTime:F3} ms");
-// totalTime += dayTotalTime;
-//
-// //DAY 8
-// var day8 = new Day<int, long>(8, new Day8Part1(), new Day8Part2());
-// dayTotalTime = day8.Solve();
-// Console.WriteLine($"Day total runtime: {dayTotalTime:F3} ms");
-// totalTime += dayTotalTime;
-//
-// //DAY 9
-// var day9 = new Day<long, long>(9, new Day9Part1(), new Day9Part2());
-// dayTotalTime = day9.Solve();
-// Console.WriteLine($"Day total runtime: {dayTotalTime:F3} ms");
-// totalTime += dayTotalTime;
-//
-// //DAY 10
-// var day10 = new Day<int, int>(10, new Day10Part1(), new Day10Part2());
-// dayTotalTime = day10.Solve();
-// Console.WriteLine($"Day total runtime: {dayTotalTime:F3} ms");
-// totalTime += dayTotalTime;
+//DAY 1
+var day1 = new Day<int, int>(1, new Day1Part1(), new Day1Part2());
+dayTotalTime = day1.Solve();
+Console.WriteLine($"Day total runtime: {dayTotalTime:F3} ms");
+totalTime += dayTotalTime;
 
-// //DAY 11
-// var day11 = new Day<int, long>(11, new Day11Part1(), new Day11Part2());
-// dayTotalTime = day11.Solve();
-// Console.WriteLine($"Day total runtime: {dayTotalTime:F3} ms");
-// totalTime += dayTotalTime;
+//DAY 2
+var day2 = new Day<ulong, ulong>(2, new Day2Part1(), new Day2Part2());
+dayTotalTime = day2.Solve();
+Console.WriteLine($"Day total runtime: {dayTotalTime:F3} ms");
+totalTime += dayTotalTime;
+
+//DAY 3
+var day3 = new Day<int, long>(3, new Day3Part1(), new Day3Part2());
+dayTotalTime = day3.Solve();
+Console.WriteLine($"Day total runtime: {dayTotalTime:F3} ms");
+totalTime += dayTotalTime;
+
+//DAY 4
+var day4 = new Day<int, int>(4, new Day4Part1(), new Day4Part2());
+dayTotalTime = day4.Solve();
+Console.WriteLine($"Day total runtime: {dayTotalTime:F3} ms");
+totalTime += dayTotalTime;
+
+//DAY 5
+var day5 = new Day<int, long>(5, new Day5Part1(), new Day5Part2());
+dayTotalTime = day5.Solve();
+Console.WriteLine($"Day total runtime: {dayTotalTime:F3} ms");
+totalTime += dayTotalTime;
+
+//DAY 6
+var day6 = new Day<long, long>(6, new Day6Part1(), new Day6Part2());
+dayTotalTime = day6.Solve();
+Console.WriteLine($"Day total runtime: {dayTotalTime:F3} ms");
+totalTime += dayTotalTime;
+
+//DAY 7
+var day7 = new Day<int, long>(7, new Day7Part1(), new Day7Part2());
+dayTotalTime = day7.Solve();
+Console.WriteLine($"Day total runtime: {dayTotalTime:F3} ms");
+totalTime += dayTotalTime;
+
+//DAY 8
+var day8 = new Day<int, long>(8, new Day8Part1(), new Day8Part2());
+dayTotalTime = day8.Solve();
+Console.WriteLine($"Day total runtime: {dayTotalTime:F3} ms");
+totalTime += dayTotalTime;
+
+//DAY 9
+var day9 = new Day<long, long>(9, new Day9Part1(), new Day9Part2());
+dayTotalTime = day9.Solve();
+Console.WriteLine($"Day total runtime: {dayTotalTime:F3} ms");
+totalTime += dayTotalTime;
+
+//DAY 10
+var day10 = new Day<int, int>(10, new Day10Part1(), new Day10Part2());
+dayTotalTime = day10.Solve();
+Console.WriteLine($"Day total runtime: {dayTotalTime:F3} ms");
+totalTime += dayTotalTime;
+
+//DAY 11
+var day11 = new Day<int, long>(11, new Day11Part1(), new Day11Part2());
+dayTotalTime = day11.Solve();
+Console.WriteLine($"Day total runtime: {dayTotalTime:F3} ms");
+totalTime += dayTotalTime;
 
 //DAY 12
-var day12 = new Day<int, int>(12, new Day12Part1(), new Day12Part2());
+var day12 = new Day<int, int>(12, new Day12Part1());
 dayTotalTime = day12.Solve();
 Console.WriteLine($"Day total runtime: {dayTotalTime:F3} ms");
 totalTime += dayTotalTime;
